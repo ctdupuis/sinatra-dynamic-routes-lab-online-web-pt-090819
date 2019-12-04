81717e0cb9b3c2aea337031606ad641c79092ab9
Cody Dupuis <ctdupuis7@gmail.com>
2019-12-04 21:46:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,10 +32,13 @@ class App < Sinatra::Base
     
     case params[:operation]
     when 'add'
-      answer = number1 + number 2
+      answer = (number1 + number2).to_s
     when 'subtract'
+      answer = (number1 - number2).to_s
     when 'multiply'
+      answer = (number1 * number2).to_s
     when 'divide'
+      answer = (number1 / number2).to_s
     end
   end
 end
