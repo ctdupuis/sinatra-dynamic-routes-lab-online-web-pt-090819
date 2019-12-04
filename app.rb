082baade1ad02fc4615da4a27a50fa2e709940ab
Cody Dupuis <ctdupuis7@gmail.com>
2019-12-04 21:22:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
-    @name = params[:name].to_a.map{|a| a.reverse}.to_s
+    @name = params[:name]
     "#{@name}"
   end
 end
